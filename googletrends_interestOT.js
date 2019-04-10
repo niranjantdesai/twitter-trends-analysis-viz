a6873feaa6a0f5acc694d358e63a3bfcbe38cfe2
Niranjan Thakurdesai <nthakurdesai3@gatech.edu>
2019-04-10 09:56:40 -0400

    Modified Google Trends script to parse cleaned csv

--- googletrends_interestOT.js
+++ googletrends_interestOT.js
@@ -4,12 +4,12 @@ var Converter = require("csvtojson").Converter;
 var converter = new Converter({});
 var csvWriter = require('csv-write-stream')
 var fastcsv = require('fast-csv');
+var path = require('path');
 var dataArray = [];
 var trend_names = [];
 var all = [];
 
-// const csvFilePath='remaining.csv'
-const csvFilePath='timeStamps.csv'
+const csvFilePath = path.join(__dirname, '..', 'data', 'cleaned.csv')
 const csv=require('csvtojson')
 
 var lookback = 15
@@ -19,43 +19,23 @@ new Promise(function(resolve, reject) {
   const json = csv().fromStream(stream);
   resolve(json);
 
-}).then(function(result) {
-  for(var i = 0; i < result.length; i++) {
-    var obj = result[i];
-    var split_startDate = obj.startDate.split(" ")[0].split("/");
-    var split_endDate = obj.endDate.split(" ")[0].split("/");
-    var startDate = '20'+split_startDate[2]+'-'+split_startDate[0]+'-'+split_startDate[1]
-    var endDate = '20'+split_endDate[2]+'-'+split_endDate[0]+'-'+split_endDate[1]
-    var startDateOriginal = new Date(startDate);
-    startDateOriginal.setDate(startDateOriginal.getDate() - lookback);   // lookback
-    startDateOriginal = new Date(startDateOriginal.toUTCString().substr(0, 25))
-    // var endDateOriginal = new Date(endDate);
-    var endDateOriginal = new Date(new Date(endDate).toUTCString().substr(0, 25))
-    result[i].startDate = startDateOriginal
-    result[i].endDate = endDateOriginal
-    all.push([result[i].Trend, startDateOriginal, endDateOriginal])
-  }
-  return result
-
-}).then(async function(result) { // (***)
+}).then(async function(result) {
   var promises = [];
 
-
-  // for (var i = 0, p = Promise.resolve(); i < result.length; i++) {
-  for (var i = 1000, p = Promise.resolve(); i < result.length; i++) {
-  // for (var i = 700, p = Promise.resolve(); i < 800; i++) {
-    trend_name = result[i].Trend
+  for (var i = 0, p = Promise.resolve(); i < result.length; i++) {
+    trend_name = result[i].Term
     console.log(trend_name)
+    var startDate = new Date(result[i].start)
+    startDate.setDate(startDate.getDate() - lookback)
+    var endDate = new Date(result[i].end)
     var success = true
  
-    await interestOverTime(result[i].Trend, result[i].startDate, result[i].endDate)
+    await interestOverTime(result[i].Term, startDate, endDate)
       .then(function(api_results){
         trend_names.push(trend_name);
         promises.push(api_results);
-        // console.log("promises", promises)
       })
       .catch((err) => {
-        // console.log("promises", promises)
         var index = 0
         var headers = ["trend"]
         var timestamps = []
@@ -74,10 +54,8 @@ new Promise(function(resolve, reject) {
         }
 
         writer.pipe(fs.createWriteStream('temp.csv', {flags: 'a'}))
-        // writer.write("hello bro")
         for (var i = 0; i < promises.length; i++) {
           var obj = JSON.parse(promises[i])
-          // console.log(obj.default.timelineData)
           var keysArray = obj.default.timelineData
           for (var j = 0; j < keysArray.length; j++) {
             val = []
@@ -106,14 +84,12 @@ new Promise(function(resolve, reject) {
   return Promise.all(promises)
 
 }).then(function(result) {
-  // console.log(result)
   var values = []
   var super_values = []
   var temp = {
       table:[]
   };
 
-  // console.log(all)
   var index = 0
   var headers = ["trend"]
   var timestamps = []
@@ -142,17 +118,6 @@ new Promise(function(resolve, reject) {
   return temp;
 
 });
-// .then(function(result) {
-//   console.log(result.table)
-//   console.log(all)
-//   // fs.writeFile('output.csv', result.table, 'utf8', function (err) {
-//   //   if (err) {
-//   //     console.log('Some error occured - file either not saved or corrupted file saved.');
-//   //   } else{
-//   //     console.log('It\'s saved!');
-//   //   }
-//   // });
-// });
 
 function saveTempResult(result) {
   console.log("arrived")
@@ -161,26 +126,11 @@ function saveTempResult(result) {
   for (var i = 0; i < result.length; i++) {
     console.log(result[i])
   }
-   // return new Promise(function(resolve, reject){
-   //    // var api_return = googleTrends.interestOverTime({keyword: trend, startTime: startDate, endTime: endDate});
-   //    // return resolve(api_return);
-   //    // return reject(error);
-   //    googleTrends.interestOverTime({keyword: trend, startTime: startDate, endTime: endDate})
-   //    .then(function(results){
-   //      return resolve(results);
-   //    })
-   //    .catch(function(err){
-   //      return reject(err);
-   //    })
-   // });
 }
 
 
 function interestOverTime(trend, startDate, endDate) {
    return new Promise(function(resolve, reject){
-      // var api_return = googleTrends.interestOverTime({keyword: trend, startTime: startDate, endTime: endDate});
-      // return resolve(api_return);
-      // return reject(error);
       googleTrends.interestOverTime({keyword: trend, startTime: startDate, endTime: endDate})
       .then(function(results){
         return resolve(results);

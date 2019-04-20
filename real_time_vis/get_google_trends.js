39dd25c63ec233470f420414a776303f0c6faee7
Niranjan Thakurdesai <nthakurdesai3@gatech.edu>
2019-04-20 00:01:10 -0400

    Added Python requirements file, node scripts to get Google trends

---
+++ real_time_vis/get_google_trends.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const googleTrends = require('google-trends-api');
+const csv = require('csvtojson');
+var csvWriter = require('csv-write-stream')
+
+const csvFilePath = process.argv[2];
+console.log(csvFilePath);
+var lookback = 15;
+var dataArray = [];
+var trend_names = [];
+
+// read the csv with the current Twitter trends
+new Promise(function(resolve, reject) {
+  const stream = fs.createReadStream(csvFilePath);
+  const json = csv().fromStream(stream);
+  resolve(json);
+})
+// iterate over all trends and get Google Trends interest over time for each trend
+.then(async function(result) {
+  var promises = [];
+
+  for (var i = 0, p = Promise.resolve(); i < result.length; i++) {
+    trend_name = result[i].name;
+    // console.log(trend_name)
+
+    // start time is the current time minus the lookback period, end time is the
+    // current time
+    var startDate = new Date();
+    startDate.setDate(startDate.getDate() - lookback);
+    var endDate = new Date();
+    var success = true;
+    
+    // get Google Trends interest over time for the current trend in the list
+    await interestOverTime(trend_name, startDate, endDate)
+      .then(function(api_results){
+        trend_names.push(trend_name);
+        promises.push(api_results);
+      })
+      .catch((err) => {
+        return reject(err);
+      });
+  }
+  return Promise.all(promises)
+})
+// save results to a csv
+.then(function(result) {
+  // write headers
+  var headers = ["trend"];
+  var timestamps = [];
+  for(var t = lookback; t > 0; t--)
+  {
+    headers.push("t-" + t);
+    timestamps.push("NaN");
+  }
+
+  headers.push("t");
+  timestamps.push("NaN");
+  var writer = csvWriter({ headers: headers});
+
+  // write to csv
+  writer.pipe(fs.createWriteStream('google_trends.csv'));
+  for (var i = 0; i < result.length; i++) {
+    var obj = JSON.parse(result[i]);
+    var keysArray = obj.default.timelineData;
+    for (var j = 0; j < keysArray.length; j++) {
+      val = [keysArray[j].formattedTime, keysArray[j].formattedValue[0]];
+      timestamps[j] = keysArray[j].formattedValue[0];
+    }
+    writer.write([trend_names[i]].concat(timestamps));
+  }
+  writer.end();
+});
+
+// Get Google Trends interest over time for a trend and given time period
+function interestOverTime(trend, startDate, endDate) {
+   return new Promise(function(resolve, reject){
+      googleTrends.interestOverTime({keyword: trend, startTime: startDate, endTime: endDate})
+      .then(function(results){
+        return resolve(results);
+      })
+      .catch(function(err){
+        return reject(err);
+      })
+   });
+}
---
+++ scripts/googletrends_interestOT.js
@@ -0,0 +1,143 @@
+const fs = require('fs');
+const googleTrends = require('google-trends-api');
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+var csvWriter = require('csv-write-stream')
+var fastcsv = require('fast-csv');
+var path = require('path');
+var dataArray = [];
+var trend_names = [];
+var all = [];
+
+const csvFilePath = path.join(__dirname, '..', 'data', 'cleaned0.csv')
+const csv=require('csvtojson')
+
+var lookback = 15
+
+new Promise(function(resolve, reject) {
+  const stream = fs.createReadStream(csvFilePath);
+  const json = csv().fromStream(stream);
+  resolve(json);
+
+}).then(async function(result) {
+  var promises = [];
+
+  // for (var i = 0, p = Promise.resolve(); i < result.length; i++) {
+  for (var i = 3188, p = Promise.resolve(); i < result.length; i++) {
+    trend_name = result[i].Term
+    console.log(trend_name)
+    var startDate = new Date(result[i].start)
+    startDate.setDate(startDate.getDate() - lookback)
+    var endDate = new Date(result[i].end)
+    var success = true
+ 
+    await interestOverTime(result[i].Term, startDate, endDate)
+      .then(function(api_results){
+        trend_names.push(trend_name);
+        promises.push(api_results);
+      })
+      .catch((err) => {
+        var index = 0
+        var headers = ["trend"]
+        var timestamps = []
+        for(var t = lookback; t > 0; t--)
+        {
+          headers.push("t-" + t)
+          timestamps.push("NaN")
+        }
+        headers.push("t")
+        timestamps.push("NaN")
+
+        if (!fs.existsSync('temp.csv')){
+          var writer = csvWriter({ headers: headers});
+        } else {
+          var writer = csvWriter({sendHeaders: false});
+        }
+
+        writer.pipe(fs.createWriteStream('temp.csv', {flags: 'a'}))
+        for (var i = 0; i < promises.length; i++) {
+          var obj = JSON.parse(promises[i])
+          var keysArray = obj.default.timelineData
+          for (var j = 0; j < keysArray.length; j++) {
+            val = []
+            val = [keysArray[j].formattedTime, keysArray[j].formattedValue[0]]
+            timestamps[j] = keysArray[j].formattedValue[0]
+          }
+          writer.write([trend_names[i]].concat(timestamps))
+        }
+        writer.end()
+
+        console.log('promise rejected')
+        console.log(err)
+        // // decrement index because we have to query it again
+        // --i;
+        success = false
+      });
+
+      if(!success)
+        break
+
+      // nasty hack to wait
+      var wait_seconds = 1
+      var waitTill = new Date(new Date().getTime() + wait_seconds * 1000)
+      while(waitTill > new Date()){}
+  }
+  return Promise.all(promises)
+
+}).then(function(result) {
+  var values = []
+  var super_values = []
+  var temp = {
+      table:[]
+  };
+
+  var index = 0
+  var headers = ["trend"]
+  var timestamps = []
+  for(var t = lookback; t > 0; t--)
+  {
+    headers.push("t-" + t)
+    timestamps.push("NaN")
+  }
+  headers.push("t")
+  timestamps.push("NaN")
+
+  var writer = csvWriter({ headers: headers})
+  writer.pipe(fs.createWriteStream('google_trends_5801.csv'))
+  for (var i = 0; i < result.length; i++) {
+    var obj = JSON.parse(result[i])
+    var keysArray = obj.default.timelineData
+    for (var j = 0; j < keysArray.length; j++) {
+      val = []
+      val = [keysArray[j].formattedTime, keysArray[j].formattedValue[0]]
+      timestamps[j] = keysArray[j].formattedValue[0]
+    }
+    writer.write([trend_names[i]].concat(timestamps))
+  }
+  writer.end()
+
+  return temp;
+
+});
+
+function saveTempResult(result) {
+  console.log("arrived")
+  console.log(result)
+  console.log("length of result", result.length)
+  for (var i = 0; i < result.length; i++) {
+    console.log(result[i])
+  }
+}
+
+
+function interestOverTime(trend, startDate, endDate) {
+   return new Promise(function(resolve, reject){
+      googleTrends.interestOverTime({keyword: trend, startTime: startDate, endTime: endDate})
+      .then(function(results){
+        return resolve(results);
+      })
+      .catch(function(err){
+        return reject(err);
+      })
+   });
+}
